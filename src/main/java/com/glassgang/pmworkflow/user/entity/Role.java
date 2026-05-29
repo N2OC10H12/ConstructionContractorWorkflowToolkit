2e2ebce372db6d48b1fd21a76cf90c3e5f1321a9
Vladimir Belov <vlad@local.arrowglass.com>
2026-05-29 16:11:11 -0500

    improvements

--- src/main/java/com/glassgang/pmworkflow/user/entity/Role.java
+++ src/main/java/com/glassgang/pmworkflow/user/entity/Role.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 public enum Role {
     ADMIN,
+
     PM,
-    PROJECT_VIEWER,
-    SUPERVISOR,
+    PM_MANAGER,
+    PM_VIEWER,
+
+    SUPERVISOR, // temporary legacy role, remove later
+
     ESTIMATOR,
-    ESTIMATOR_SUPERVISOR,
+    ESTIMATE_MANAGER,
     ESTIMATE_VIEWER;
-    
+
     public static Role from(String value) {
         if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("Role is required");
@@ -26,11 +30,71 @@ public enum Role {
         return this == ADMIN;
     }
 
+    public boolean isPm() {
+        return this == PM;
+    }
+
+    public boolean isPmManager() {
+        return this == PM_MANAGER;
+    }
+
+    public boolean isPmViewer() {
+        return this == PM_VIEWER;
+    }
+
     public boolean isSupervisor() {
         return this == SUPERVISOR;
     }
 
-    public boolean isPm() {
-        return this == PM;
+    public boolean isEstimator() {
+        return this == ESTIMATOR;
+    }
+
+    public boolean isEstimateManager() {
+        return this == ESTIMATE_MANAGER;
+    }
+
+    public boolean isEstimateViewer() {
+        return this == ESTIMATE_VIEWER;
+    }
+
+    public boolean canViewAllProjects() {
+        return this == ADMIN
+                || this == PM_MANAGER
+                || this == PM_VIEWER
+                || this == SUPERVISOR;
+    }
+
+    public boolean canManageProjects() {
+        return this == ADMIN
+                || this == PM_MANAGER;
+    }
+
+    public boolean canWorkOwnProjects() {
+        return this == ADMIN
+                || this == PM
+                || this == PM_MANAGER;
+    }
+
+    public boolean canViewAllEstimates() {
+        return this == ADMIN
+                || this == ESTIMATE_MANAGER
+                || this == ESTIMATE_VIEWER;
+    }
+
+    public boolean canWorkOwnEstimates() {
+        return this == ADMIN
+                || this == ESTIMATOR
+                || this == ESTIMATE_MANAGER;
+    }
+
+    public boolean canManageEstimateDictionaries() {
+        return this == ADMIN
+                || this == ESTIMATE_MANAGER;
+    }
+
+    public boolean canConvertEstimateToProject() {
+        return this == ADMIN
+                || this == ESTIMATE_MANAGER;
     }
 }
